d2a787b938d9a4953169a47951d8983a4562c343
Vitor Jose <vitorcosta-pm@hotmail.com>
2024-05-30 13:07:50 -0300

    fix: remove unused method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -69,19 +69,4 @@ export class AppController {
       data,
     };
   }
-
-  @ApiResponse({
-    status: 200,
-    description:
-      'Pokémon are the creatures that inhabit the world of the Pokémon games. They can be caught using Pokéballs and trained by battling with other Pokémon. Each Pokémon belongs to a specific species but may take on a variant which makes it differ from other Pokémon of the same species, such as base stats, available abilities and typings',
-  })
-  @Get('/findByName')
-  public findByName(@Req() req: Request, @Query('query') query?: string) {
-    this.logger.log({
-      httpEndpoint: req.path,
-      message: 'request received',
-    });
-
-    return this.appService.getByName(query);
-  }
 }
